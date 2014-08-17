07c015f95535b0ee7d437b4ecc490c1f52877e7b
singularengineer <singularengineer@outlook.com>
2014-08-16 22:00:34 -0400

    Update for the new MLA
    these file are updated to the microchip's new MLA

--- framework/usb/usb_device.h
+++ framework/usb/usb_device.h
@@ -1,31 +1,47 @@
-//DOM-IGNORE-BEGIN
 /*******************************************************************************
-Software License Agreement
+ Module for Microchip USB Library
 
-The software supplied herewith by Microchip Technology Incorporated
-(the "Company") for its PICmicro(R) Microcontroller is intended and
-supplied to you, the Company's customer, for use solely and
-exclusively on Microchip PICmicro Microcontroller products. The
-software is owned by the Company and/or its supplier, and is
-protected under applicable copyright laws. All rights are reserved.
-Any use in violation of the foregoing restrictions may subject the
-user to criminal sanctions under applicable laws, as well as to
-civil liability for the breach of the terms and conditions of this
-license.
+  Company:
+    Microchip Technology Inc.
 
-THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
-WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
-TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
-IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
-CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+  File Name:
+    usb_device.h
 
+  Summary:
+    Defines types and APIs associated with the USB device stack.
+
+  Description:
+    Defines types and APIs associated with the USB device stack.
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2014 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
 *******************************************************************************/
 //DOM-IGNORE-END
 
 //DOM-IGNORE-BEGIN
-#ifndef USBDEVICE_H
-#define USBDEVICE_H
+#ifndef _USB_DEVICE_H
+#define _USB_DEVICE_H
 //DOM-IGNORE-END
 
 #include <usb/usb_common.h>
--- src/system.c
+++ src/system.c
@@ -25,28 +25,47 @@
 #include <usb/usb.h>
 
 /** CONFIGURATION Bits **********************************************/
-#pragma config PLLSEL   = PLL3X     // 3X PLL multiplier selected
-#pragma config CFGPLLEN = OFF       // PLL turned on during execution
-#pragma config CPUDIV   = NOCLKDIV  // 1:1 mode (for 48MHz CPU)
-#pragma config LS48MHZ  = SYS48X8   // Clock div / 8 in Low Speed USB mode
-#pragma config FOSC     = INTOSCIO  // HFINTOSC selected at powerup, no clock out
-#pragma config PCLKEN   = OFF       // Primary oscillator driver
-#pragma config FCMEN    = OFF       // Fail safe clock monitor
-#pragma config IESO     = OFF       // Internal/external switchover (two speed startup)
-#pragma config nPWRTEN  = OFF       // Power up timer
-#pragma config BOREN    = SBORDIS   // BOR enabled
-#pragma config nLPBOR   = ON        // Low Power BOR
-#pragma config WDTEN    = SWON      // Watchdog Timer controlled by SWDTEN
-#pragma config WDTPS    = 32768     // WDT postscalar
-#pragma config PBADEN   = OFF       // Port B Digital/Analog Powerup Behavior
-#pragma config SDOMX    = RC7       // SDO function location
-#pragma config LVP      = OFF       // Low voltage programming
-#pragma config MCLRE    = ON        // MCLR function enabled (RE3 disabled)
-#pragma config STVREN   = ON        // Stack overflow reset
-//#pragma config ICPRT  = OFF       // Dedicated ICPORT program/debug pins enable
-#pragma config XINST    = OFF       // Extended instruction set
-
-
+#pragma config PLLSEL   = PLL3X     // PLL Selection (3x clock multiplier)
+#pragma config CFGPLLEN = OFF       // PLL Enable Configuration bit (PLL Disabled (firmware controlled))
+#pragma config CPUDIV   = NOCLKDIV  // CPU System Clock Postscaler (CPU uses system clock (no divide))
+#pragma config LS48MHZ  = SYS48X8   // Low Speed USB mode with 48 MHz system clock (System clock at 48 MHz, USB clock divider is set to 8)
+#pragma config FOSC     = INTOSCIO  // Oscillator Selection (Internal oscillator)
+#pragma config PCLKEN   = OFF       // Primary Oscillator Shutdown (Primary oscillator shutdown firmware controlled)
+#pragma config FCMEN    = OFF       // Fail-Safe Clock Monitor (Fail-Safe Clock Monitor disabled)
+#pragma config IESO     = OFF       // Internal/External Oscillator Switchover (Oscillator Switchover mode disabled)
+#pragma config nPWRTEN  = OFF       // Power-up Timer Enable (Power up timer disabled)
+#pragma config BOREN    = SBORDIS   // Brown-out Reset Enable (BOR enabled in hardware (SBOREN is ignored))
+#pragma config BORV     = 190       // Brown-out Reset Voltage (BOR set to 1.9V nominal)
+#pragma config nLPBOR   = ON        // Low-Power Brown-out Reset (Low-Power Brown-out Reset enabled)
+#pragma config WDTEN    = SWON      // Watchdog Timer Enable bits (WDT controlled by firmware (SWDTEN enabled))
+#pragma config WDTPS    = 32768     // Watchdog Timer Postscaler (1:32768)
+#pragma config CCP2MX   = RC1       // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN   = OFF       // PORTB A/D Enable bit (PORTB<5:0> pins are configured as digital I/O on Reset)
+#pragma config T3CMX    = RC0       // Timer3 Clock Input MUX bit (T3CKI function is on RC0)
+#pragma config SDOMX    = RC7       // SDO Output MUX bit (SDO function is on RC7)
+#pragma config MCLRE    = ON        // Master Clear Reset Pin Enable (MCLR pin enabled; RE3 input disabled)
+#pragma config STVREN   = ON        // Stack Full/Underflow Reset (Stack full/underflow will cause Reset)
+#pragma config LVP      = OFF       // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config ICPRT    = OFF       // Dedicated In-Circuit Debug/Programming Port Enable (ICPORT disabled)
+#pragma config XINST    = OFF       // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled)
+#pragma config CP0      = OFF       // Block 0 Code Protect (Block 0 is not code-protected)
+#pragma config CP1      = OFF       // Block 1 Code Protect (Block 1 is not code-protected)
+#pragma config CP2      = OFF       // Block 2 Code Protect (Block 2 is not code-protected)
+#pragma config CP3      = OFF       // Block 3 Code Protect (Block 3 is not code-protected)
+#pragma config CPB      = OFF       // Boot Block Code Protect (Boot block is not code-protected)
+#pragma config CPD      = OFF       // Data EEPROM Code Protect (Data EEPROM is not code-protected)
+#pragma config WRT0     = OFF       // Block 0 Write Protect (Block 0 (0800-1FFFh) is not write-protected)
+#pragma config WRT1     = OFF       // Block 1 Write Protect (Block 1 (2000-3FFFh) is not write-protected)
+#pragma config WRT2     = OFF       // Block 2 Write Protect (Block 2 (04000-5FFFh) is not write-protected)
+#pragma config WRT3     = OFF       // Block 3 Write Protect (Block 3 (06000-7FFFh) is not write-protected)
+#pragma config WRTC     = OFF       // Configuration Registers Write Protect (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB     = OFF       // Boot Block Write Protect (Boot block (0000-7FFh) is not write-protected)
+#pragma config WRTD     = OFF       // Data EEPROM Write Protect (Data EEPROM is not write-protected)
+#pragma config EBTR0    = OFF       // Block 0 Table Read Protect (Block 0 is not protected from table reads executed in other blocks)
+#pragma config EBTR1    = OFF       // Block 1 Table Read Protect (Block 1 is not protected from table reads executed in other blocks)
+#pragma config EBTR2    = OFF       // Block 2 Table Read Protect (Block 2 is not protected from table reads executed in other blocks)
+#pragma config EBTR3    = OFF       // Block 3 Table Read Protect (Block 3 is not protected from table reads executed in other blocks)
+#pragma config EBTRB    = OFF       // Boot Block Table Read Protect (Boot block is not protected from table reads executed in other blocks)
 
 
 
@@ -78,8 +97,8 @@ void SYSTEM_Initialize( SYSTEM_STATE state )
                 ACTCON = 0x90;  //Enable active clock tuning for USB operation
             #endif
 			
-//            LED_Enable(LED_USB_DEVICE_STATE);
-//            BUTTON_Enable(BUTTON_DEVICE_CDC_BASIC_DEMO);
+            LED_Enable(LED_USB_DEVICE_STATE);
+            BUTTON_Enable(BUTTON_DEVICE_CDC_BASIC_DEMO);
             break;
 			
         case SYSTEM_STATE_USB_SUSPEND: 
